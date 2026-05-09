b4297c80d2b882b2445037f9ad694d2650fbecd4
lauzanabil12-hue <lauzanabil12@gmail.com>
2026-05-09 23:54:49 +0700

    add unit test

---
+++ User.Test/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xunit;
+
+namespace UserTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void CanPerform_ReturnsTrue_WhenPermissionExists()
+        {
+            
+            User user = new User(
+                Guid.NewGuid(),
+                "Rafael",
+                "08123456789"
+            );
+
+            user.AddPermission("CreateReport");
+
+            
+            bool result = user.CanPerform("CreateReport");
+
+            
+            Assert.True(result);
+        }
+    }
+}
