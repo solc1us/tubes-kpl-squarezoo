e8624789aaa2cdac8da3aff90b4a2055f5c20000
lauzanabil12-hue <lauzanabil12@gmail.com>
2026-05-11 13:16:45 +0700

    Menambahkan UnitTest User

---
+++ UserPerformence/UserPerformence.cs
@@ -0,0 +1,44 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using tubes_kpl_squarezoo;
+
+namespace UserPerformence
+{
+    [MemoryDiagnoser]
+    public class UserPerformence
+    {
+        public User user;
+
+        [IterationSetup]
+        public void Setup()
+        {
+            user = new User(
+                Guid.NewGuid(),
+                "Rafael",
+                "08123456789"
+            );
+
+            user.AddPermission("CreateReport");
+            user.AddPermission("ViewReport");
+        }
+
+        [Benchmark]
+        public bool CanPerformBenchmark()
+        {
+            return user.CanPerform("CreateReport");
+        }
+
+        [Benchmark]
+        public void AddPermissionBenchmark()
+        {
+            user.AddPermission("NewPermission");
+        }
+
+        [Benchmark]
+        public List<string> GetPermissionsBenchmark()
+        {
+            return user.GetPermissions();
+        }
+    }
+}
